96aa1b6bec7f4fcc8d56a717112dab242544a045
Bomjun Kwon <bomjun.kwon@garmin.com>
2020-02-27 16:29:36 -0800

    Revert "changes for vs2019; removing redundancy in cmplx.c"
    This reverts commit 55e6b41581fca180ded217290c1a07c6ccfded72.

--- sigproc/iir/cmplx.c
+++ sigproc/iir/cmplx.c
@@ -449,7 +449,9 @@ w->r *= 0.5;
 w->i *= 0.5;
 }
 
-// Obsolte; must be removed to avoid conflict in Visual Studio 2019
+//Not used in the iir project. Probably obsolte and the author forget to remove
+//Commented out because it conflicts with hypot when using Visual Studio 2019
+// 1/27/2020 bjkwon
 //double hypot( x, y )
 //double x, y;
 //{
